bc36da8156d6233b5c43d46a918e7b44293ea978
gimi65536 <gimi65536@hotmail.com>
2018-01-19 20:58:32 +0800

    A部分補完
    common case的新規則補完，perl文件的A08 A09待補。

--- check template.pl
+++ check template.pl
@@ -13,32 +13,41 @@ sub signtoregex{
 }
 
 #one-char operators
-@baseplusope   = qw(+ -);
-@basemultiope  = qw(* / %);
-@baseexpope    = qw(^);
-@baseparentope = qw/ ( ) /;
-@baseassignope = qw(=);
-@baseope = (@baseplusope, @basemultiope, @baseexpope, @baseparentope, @baseassignope);
+@baseplusope        = qw(+ -);
+@basemultiope       = qw(* / %);
+@baseexpope         = qw(^);
+@baseleftparentope  = qw/(/;
+@baserightparentope = qw/)/;
+@baseassignope      = qw(=);
+@baseope = (@baseplusope, @basemultiope, @baseexpope, @baseleftparentope, @baserightparentope, @baseassignope);
 
 #multi-char operators
-%pairplusope   = ();
-%pairmultiope  = ();
-%pairexpope    = ();
-%pairparentope = ();
-%pairassignope = ('+=' => '`', '-=' => '!', '*=' => '@', '/=' => '\\', '%=' => '&');
+%pairplusope        = ();
+%pairmultiope       = ();
+%pairexpope         = ();
+%pairleftparentope  = ();
+%pairrightparentope = ();
+%pairassignope      = ('+=' => '`', '-=' => '!', '*=' => '@', '/=' => '\\', '%=' => '&');
 %pairope = (%pairplusope, %pairmultiope, %pairexpope, %pairparentope, %pairassignope);
 
 #operator grouped
-@plusope   = (@baseplusope,   values %pairplusope);
-@multiope  = (@basemultiope,  values %pairmultiope);
-@expope    = (@baseexpope,    values %pairexpope);
-@parentope = (@baseparentope, values %pairparentope);
-@assignope = (@baseassignope, values %pairassignope);
-@ope = (@plusope, @multiope, @expope, @parentope, @assignope);
+@plusope        = (@baseplusope,        values %pairplusope);
+@multiope       = (@basemultiope,       values %pairmultiope);
+@expope         = (@baseexpope,         values %pairexpope);
+@leftparentope  = (@baseleftparentope,  values %pairleftparentope);
+@rightparentope = (@baserightparentope, values %pairrightparentope);
+@assignope      = (@baseassignope,      values %pairassignope);
+@ope = (@plusope, @multiope, @expope, @leftparentope, @rightparentope, @assignope);
 
 #operator in regex
-$baseope_regex = &signtoregex(@baseope);
-$ope_regex     = &signtoregex(@ope);
+$baseope_regex        = &signtoregex(@baseope);
+$plusope_regex        = &signtoregex(@plusope);
+$multiope_regex       = &signtoregex(@multiope);
+$expope_regex         = &signtoregex(@expope);
+$leftparentope_regex  = &signtoregex(@leftparentope);
+$rightparentope_regex = &signtoregex(@rightparentope);
+$assignope_regex      = &signtoregex(@assignope);
+$ope_regex            = &signtoregex(@ope);
 %pairope_regex = map{
 	my ($tmp, $value) = ($_, $pairope{$_});
 	&signtoregex($_) => &signtoregex($value)
@@ -47,7 +56,8 @@ $ope_regex     = &signtoregex(@ope);
 $var_regex     = '\w\$'; #A-Za-z0-9_
 $varinit_regex = 'A-Za-z_\$';
 
-$s = "  4a23a23 aaa111_\$+ab+a'23'5 2a ```+=5{-|1|+|1waifu} += 5-=5*=5/=5%=5aaaaaa";
+#$s = "  4a23a23 aaa111_\$+ab+a'23'5 2a ```+=5{-|1|+|1waifu} += 5-=5*=5/=5%=5aaaaaa";
+$s = "5=(a += 5";
 say $s;
 #A00.1
 #$s =~ s/[^\w\$\s+\-*\/%^\(\)=]//g;
@@ -56,7 +66,7 @@ say $s;
 #A00.2
 foreach $i (keys %pairope){
 	my $tmp = &signtoregex($i);
-	$s =~ s/$tmp/$pairope{$i}/g;
+	$s =~ s/$tmp/${pairope{$i}}/g;
 }
 say $s;
 #A00.3
@@ -72,3 +82,16 @@ $s =~ s/^\s+|\s+$|([^\s]\s)\s+(?=[^\s])/$1$2/g;
 say $s;
 
 say "A01 error" if $s !~ /[^\s]/;
+say "A02 error" if $s =~ /^[^$var_regex$plusope_regex$leftparentope_regex]/;
+say "A03 error" if $s =~ /[^$var_regex$rightparentope_regex]$/;
+say "A04 error" if $s =~ /[$plusope_regex$multiope_regex$expope_regex$leftparentope_regex$assignope_regex]\s[$multiope_regex$expope_regex]/;
+say "A05 error" if $s =~ /[^$var_regex$rightparentope_regex]\s[$rightparentope_regex]/;
+say "A06 error" if $s =~ /[^$var_regex]\s[$assignope_regex]/;
+say "A07 error" if $s =~ /[^$var_regex$leftparentope_regex$assignope_regex\s].*?[$assignope_regex]/;
+
+#@a;
+#push @a, [$1, $2] while ($s =~ /(a)(2)/g);
+#print $a[0][0];
+#while($s =~ /(a)(2)/g){
+#	print $1;
+#}
