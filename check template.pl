fc8fe46b7d1f0552ad1b18628c6d9516f29358fe
gimi65536 <gimi65536@hotmail.com>
2018-01-19 16:44:27 +0800

    A01新增，Perl代碼的整理
    將原本出現在A12的「不得全白」條文挪到A01，畢竟是這麼基礎的條件。
    對作為prototype的Perl代碼在擴充性與排版方面進行整理了。

--- check template.pl
+++ check template.pl
@@ -4,34 +4,71 @@ use warnings;
 use 5.010;
 
 sub signtoregex{
-	my ($s) = @_;
-	$s =~ s/([^\w\$])/\\$1/g;
+	my $s = join("", map {
+		my $tmp = $_; #copy, or you would modify original data
+		$tmp =~ s/([^\w\$])/\\$1/g;
+		$tmp
+	} @_);
 	return $s;
 }
-@baseope = qw(+ - * / % ^ ( ) =);
-%pairope = ('+=' => '`', '-=' => '!', '*=' => '@', '/=' => '\\', '%=' => '&');
-@ope = (@baseope, values %pairope);
-#print @ope, "\n";
-@opeinregex = map {&signtoregex($_)} @ope;
-#print @opeinregex, "\n";
-%pairopeinregex = map{
+
+#one-char operators
+@baseplusope   = qw(+ -);
+@basemultiope  = qw(* / %);
+@baseexpope    = qw(^);
+@baseparentope = qw/ ( ) /;
+@baseassignope = qw(=);
+@baseope = (@baseplusope, @basemultiope, @baseexpope, @baseparentope, @baseassignope);
+
+#multi-char operators
+%pairplusope   = ();
+%pairmultiope  = ();
+%pairexpope    = ();
+%pairparentope = ();
+%pairassignope = ('+=' => '`', '-=' => '!', '*=' => '@', '/=' => '\\', '%=' => '&');
+%pairope = (%pairplusope, %pairmultiope, %pairexpope, %pairparentope, %pairassignope);
+
+#operator grouped
+@plusope   = (@baseplusope,   values %pairplusope);
+@multiope  = (@basemultiope,  values %pairmultiope);
+@expope    = (@baseexpope,    values %pairexpope);
+@parentope = (@baseparentope, values %pairparentope);
+@assignope = (@baseassignope, values %pairassignope);
+@ope = (@plusope, @multiope, @expope, @parentope, @assignope);
+
+#operator in regex
+$baseope_regex = &signtoregex(@baseope);
+$ope_regex     = &signtoregex(@ope);
+%pairope_regex = map{
 	my ($tmp, $value) = ($_, $pairope{$_});
 	&signtoregex($_) => &signtoregex($value)
 } keys %pairope;
-#print values %pairopeinregex, "\n";
+
+$var_regex     = '\w\$'; #A-Za-z0-9_
+$varinit_regex = 'A-Za-z_\$';
 
 $s = "  4a23a23 aaa111_\$+ab+a'23'5 2a ```+=5{-|1|+|1waifu} += 5-=5*=5/=5%=5aaaaaa";
-print $s, "\n";
-$s =~ s/[^\w\$\s+\-*\/%^\(\)=]//g;
-print $s, "\n";
+say $s;
+#A00.1
+#$s =~ s/[^\w\$\s+\-*\/%^\(\)=]//g;
+$s =~ s/[^${var_regex}${baseope_regex}\s]//g;
+say $s;
+#A00.2
 foreach $i (keys %pairope){
 	my $tmp = &signtoregex($i);
 	$s =~ s/$tmp/$pairope{$i}/g;
 }
-print $s, "\n";
-$s =~ s/([+\-*\/%^=\(\)^`!@\\&])/ $1 /g;
-print $s, "\n";
-$s =~ s/([A-Za-z_\$][^\s]*)(?:\s|$)/ $1  /g;
-print $s, "\n";
+say $s;
+#A00.3
+#$s =~ s/([+\-*\/%^=\(\)^`!@\\&])/ $1 /g;
+$s =~ s/([${ope_regex}])/ $1 /g;
+say $s;
+#A00.4
+#$s =~ s/([A-Za-z_\$][^\s]*)(?:\s|$)/ $1  /g;
+$s =~ s/([${varinit_regex}][^\s]*)(?:\s|$)/ $1  /g;
+say $s;
+#A00.5
 $s =~ s/^\s+|\s+$|([^\s]\s)\s+(?=[^\s])/$1$2/g;
-print $s, "\n";
+say $s;
+
+say "A01 error" if $s !~ /[^\s]/;
